e9e7c3dcf3bed0d7f39010a27f3681248c501574
mohnate <andrewclarker078@gmail.com>
2025-03-20 13:33:32 +0000

    fix: Updated Defaultlayout/index.ts

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -29,19 +29,6 @@ export function DefaultLayout() {
 
   return (
     <div className="flex relative min-h-screen w-full flex-col justify-between bg-background">
-      <div className="absolute right-6 bottom-6 z-[9999]">
-        <a
-          href="https://www.buymeacoffee.com/kayooliveira"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-            alt="Buy Me A Coffee"
-            style={{ height: '36.3px', width: '131.285px' }}
-          />
-        </a>
-      </div>
       <Navbar />
       <div className="flex h-full w-full max-w-[1400px] flex-1 overflow-auto overflow-x-hidden pt-4 lg:mx-auto">
         <div className="flex-1">{isLoading ? <Loading /> : <Outlet />}</div>
